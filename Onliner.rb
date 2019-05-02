a1948998e230d78d63b24934be99043269a39753
Qupad <capzed@gmail.com>
2019-05-02 21:36:37 +0300

    no1

--- Onliner.rb
+++ Onliner.rb
@@ -3,6 +3,9 @@ require 'open-uri'
 require 'csv'
 require 'json'
 require 'sqlite3'
+require 'parallel'
+
+@count503 = 0
 
 def wait(i,x=0)
 #0 = even ; 1 = odd .
@@ -14,88 +17,104 @@ def wait(i,x=0)
 end
 
 def valid_json?(string)
-  !!JSON.parse(string)
-rescue JSON::ParserError
-  false
+	!!JSON.parse(string)
+	rescue JSON::ParserError
+	  false
+end
+
+def parse(url)
+	return Nokogiri::HTML(open(url), nil, Encoding::UTF_8.to_s)
+	rescue OpenURI::HTTPError => error	
+		response = error.io
+		puts "\n\t			***  SHIT HERE WE GO AGAIN  ***\n\t\t\t#{response.status}\n"
+		status = response.status		
+		if status.to_s =~ /503/
+			@count503+=1
+			puts "\n\t\t\tERRORS OCCURED: #{@count503} PEACES OF SHIT\n"
+			sleep (Random.new.rand(15..24))		
+			parse(url)
+		end	
+end
+
+def parseJson(url)
+	return JSON.parse(url.text)
+	rescue JSON::ParserError => error
+		puts "\n			MEGA SHIT			\n\t\t\tUNVALID JSON\n"
+		return false
 end
 
 #db = SQLite3::Database.new 'Data.db'
 #db.execute 'CREATE TABLE "products" ("id" integer, "type" string, "name" string, "price" integer, "descrition" string)'
 
 #name = gets.chomp
-threads_m = []
-threads_p = []
-hrefs = []
+apis = []
+alpha_m = Mutex.new
+alpha_p = Mutex.new
 
 File.open("valid_api.txt",'r') do |f|
   f.each_line do |line|
-	hrefs << line.strip
+	apis << line.strip
   end
 end
 
-size = hrefs.size
-
-size.times do |i|
-	threads_m << Thread.new(i) do |thr_n|
-		url = hrefs[thr_n]
-		url_temp = url
-		hash = []
-		wait(thr_n)
-		puts thr_n
-		sleep(9999)
-		page_m = Nokogiri::HTML(open(url, 'User-Agent' => 'Yandex'))
-		sleep(5)
-		jso = JSON.parse(page_m.text)
-		sleep(5)
-		hash = jso["page"]
-		current_p = hash["current"]
-		last_p = hash["last"]
-		last_p.times do |i|
-			url_t = url_temp + "?page=" + (i+1).to_s
-			puts url_t
-			threads_p << Thread.new(url_t) do |uri|
-				href = []
-				wait(thr_n,1)
-				page_m = Nokogiri::HTML(open(uri, 'User-Agent' => 'Yandex'))
-				sleep(5)
-				jso = JSON.parse(page_m.text)
-				jso["products"].each do |uri|
-					href << uri["html_url"]
-				end
-				href.each do |uri|
-					sleep(1)
-					page = Nokogiri::HTML(open(uri), nil, Encoding::UTF_8.to_s)
-					sleep(5)
-					name = page.xpath('//*[@class="catalog-masthead__title"]/text()').text.strip
-					descr = page.xpath('//p[@itemprop="description"]/text()').text
-					price = page.xpath('//div[@class="offers-description__price offers-description__price_primary"]/a/text()').text.strip
-
-					puts name
-					puts descr
-					puts price
-
-					type = page.xpath('//ol/li[2]/a/span/text()').text
-					puts type
-					
-=begin
-					i = 0		
-					page.xpath('//*[@class="value__text"]').each do |node| 
-						i += 1
-						info = node.xpath('./text()').text
-						if i == 1 || i == 4 || i == 5 || info =~ /\d+.(ГБ)$/
-							arr << info
-						end
-					end	
-					puts arr
-=end	
-					
-					CSV.open("Onliner1.txt",'a'){ |file| 
-						file << [name,type,price,descr]
-					}	
-				end
+Parallel.map(apis, in_threads: 5, progress:"Dick => Pussy") do |url|
+	url_temp = url
+	hash = []
+	pages = []
+	page_m = parse(url)
+	jso = parseJson(page_m)
+	sleep(0.5)
+	hash = jso["page"]
+	current_p = hash["current"]
+	last_p = hash["last"]
+	last_p.times do |i|
+		pages << url_temp + "?page=" + (i+1).to_s
+	end
+	Parallel.map(pages, in_threads: 10, progress:"Page => Shit") do |uri|
+		puts "\n #{uri} \n"
+		href = []
+		jso = []
+		alpha_m.synchronize do
+			page_m = parse(uri)
+			jso = parseJson(page_m)
+			if !jso
+				next
 			end
-			threads_p.each {|thr| thr.join }
+			sleep(0.5)
+		end
+		jso["products"].each do |uri|
+			href << uri["html_url"]
+		end
+		alpha_p.synchronize do
+			href.each do |uri|
+				sleep(Random.new.rand(0.5..2))
+				page = parse(uri)
+				name = page.xpath('//*[@class="catalog-masthead__title"]/text()').text.strip
+				descr = page.xpath('//p[@itemprop="description"]/text()').text
+				price = page.xpath('//div[@class="offers-description__price offers-description__price_primary"]/a/text()').text.strip
+				type = page.xpath('//ol/li[2]/a/span/text()').text
+				
+				puts "\n #{uri} \n"
+				
+=begin
+				i = 0		
+				page.xpath('//*[@class="value__text"]').each do |node| 
+					i += 1
+					info = node.xpath('./text()').text
+					if i == 1 || i == 4 || i == 5 || info =~ /\d+.(ГБ)$/
+						arr << info
+					end
+				end	
+				puts arr
+=end	
+				
+				CSV.open("Onliner1.txt",'a'){ |file| 
+					file << [name,type,price,descr]
+				}	
+			end
 		end
 	end
 end
-threads_m.each {|thr| thr.join }
+Parallel.map(User.all) do |user|
+  raise Parallel::Break 
+end
